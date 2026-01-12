3f80856bf4c73a4a8fbfd8f3534b225461a02b0c
FrankMoore <97591779+FrancescoR98@users.noreply.github.com>
2026-01-12 14:44:46 +0100

    Update adnkronos.js

--- netlify/functions/adnkronos.js
+++ netlify/functions/adnkronos.js
@@ -3,19 +3,4 @@ exports.handler = function(event, context, callback) {
   
   var options = {
     host: 'www.adnkronos.com',
-    path: '/NewsFeed/UltimoraNoVideoJson.xml?username=mediaone&password=m3gt67i9gm'
-  };
-  
-  http.request(options, function(response) {
-    var str = '';
-    
-    response.on('data', function (chunk) {
-      str += chunk;
-    });
-    
-    response.on('end', function () {
-      var titles = [];
-      var re = /<title>([^<]{10,})<\/title>/g;
-      var match;
-      
-      while (match)
+    path: '/NewsFeed/UltimoraNoVideoJson.xml?username=mediaone&password=m3gt67i9
